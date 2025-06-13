6bbec5aaa7ecb70495eb7566c966abdeaf237486
Jeswin <jlibay1053538@gmail.com>
2025-06-13 14:04:11 +0800

    cleaned up

--- lua/config/options.lua
+++
@@ -1,6 +0,0 @@
-vim.opt.autoindent = true
-vim.opt.smartindent = true
-vim.opt.expandtab = true -- YAML requires spaces, not tabs!
-vim.opt.shiftwidth = 2 -- Most YAML style guides use 2 spaces
-vim.opt.softtabstop = 2
-vim.opt.tabstop = 2
